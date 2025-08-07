c3a04e6328840cec2e5add1a53661ccbc7cd6a01
FizaYasmeen <fizayasmeen99@gmail.com>
2025-08-07 22:19:25 +0530

    Updated files

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -3,17 +3,21 @@ import { Form, Input, message } from "antd";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Spinner from "../components/Spinner";
-const Register = () => {
-  const navigate = useNavigate();
+const Login = () => {
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   //from submit
   const submitHandler = async (values) => {
     try {
       setLoading(true);
-      await axios.post("/users/register", values);
-      message.success("Registeration Successfull");
+      const { data } = await axios.post("/users/login", values);
       setLoading(false);
-      navigate("/login");
+      message.success("login success");
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ ...data.user, password: "" })
+      );
+      navigate("/");
     } catch (error) {
       setLoading(false);
       message.error("something went wrong");
@@ -31,10 +35,8 @@ const Register = () => {
       <div className="resgister-page ">
         {loading && <Spinner />}
         <Form layout="vertical" onFinish={submitHandler}>
-          <h1>Register Form</h1>
-          <Form.Item label="Name" name="name">
-            <Input />
-          </Form.Item>
+          <h1>Login Form</h1>
+
           <Form.Item label="Email" name="email">
             <Input type="email" />
           </Form.Item>
@@ -42,8 +44,8 @@ const Register = () => {
             <Input type="password" />
           </Form.Item>
           <div className="d-flex justify-content-between">
-            <Link to="/login">Already Register ? Cleck Here to login</Link>
-            <button className="btn btn-primary">Register</button>
+            <Link to="/register">Not a user ? Click Here to regsiter</Link>
+            <button className="btn btn-primary">Login</button>
           </div>
         </Form>
       </div>
@@ -51,4 +53,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
